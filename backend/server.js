8a04b3c71fcb38159436957c577ca1171556a9c9
shru33 <shrur.33@gmail.com>
2026-06-24 18:40:01 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL || '*',
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 )
 
